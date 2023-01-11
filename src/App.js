b184fdb58d5b7af10862d8bda24ecbcd54da96b1
Juan Roma <romajuanmanuel@gmail.com>
2023-01-11 17:46:52 -0300

    pro

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Testimonials from './components/Testimonials';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="principal-container">
+      <h1>Esto es lo que dicen nuestros alumnos: </h1>
+      <Testimonials
+      nombre='Emma Bostian'
+      pais=' Sweden'
+      imagen='Emma'
+      cargo='Engineer'
+      empresa='spotify'
+      testimonio='Ive always struggled with learning JavaScript Ive taken many courses but freeCodeCamps course was the one which stuck Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify '/>
+       <Testimonials
+      nombre='Emma Bostian'
+      pais=' Sweden'
+      imagen='Emma'
+      cargo='Engineer'
+      empresa='spotify'
+      testimonio='Ive always struggled with learning JavaScript Ive taken many courses but freeCodeCamps course was the one which stuck Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify '/>
+       <Testimonials
+      nombre='Emma Bostian'
+      pais=' Sweden'
+      imagen='Emma'
+      cargo='Engineer'
+      empresa='spotify'
+      testimonio='Ive always struggled with learning JavaScript Ive taken many courses but freeCodeCamps course was the one which stuck Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify '/>
+      </div>
     </div>
   );
 }
